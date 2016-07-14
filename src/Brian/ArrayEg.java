0155495c4e582d9f4dac50a2c95d314adb4b6b95
jeevabaski <jeevabaski@gmail.com>
2016-07-14 11:55:31 -0500

    Simple Array Loop

---
+++ src/Brian/ArrayEg.java
@@ -0,0 +1,20 @@
+package Brian;
+
+import java.util.Scanner;
+
+public class ArrayEg {
+	
+	 public static void main(String[] args) {
+	        Scanner in = new Scanner(System.in);
+	        int n = in.nextInt();
+	        int[] arr = new int[n];
+	        for(int i=0; i < n; i++){
+	            arr[i] = in.nextInt();
+	        }
+	         in.close();
+	         System.out.println(arr.length);
+	        for(int i=arr.length -1 ;i>=0;i--)
+				System.out.println(arr[i]);
+	    }
+
+}
